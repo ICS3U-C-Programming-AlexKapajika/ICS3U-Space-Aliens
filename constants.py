3e7fb45d00703b458fe9d6ba3284ac54906b069c
Alex.Kapajika Ntumba <alex.kapajika.ntumba@stu.ocsb.ca>
2024-01-09 13:07:40 -0500

    CPT-03

---
+++ constants.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# Created by: Alex Kapajika
+# Created on: Dec 29, 2023
+# This module contains constants.
+
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TOTAL_NUMBER_OF_ALIENS = 5
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
+
+button_state = {"button_up": "up" , 
+                "button_just_pressed": "just pressed" , 
+                "button_still_pressed": "still pressed" , 
+                "button_released": "released"
+}
+
+RED_PALETTE = (b'\xff\xff\x00\x22\xcey\x22\xff\xff\xff\xff\xff\xff\xff\xff\xff'
+
+b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff')
