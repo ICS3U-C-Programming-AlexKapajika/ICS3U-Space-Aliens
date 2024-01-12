c3821df485f5a4dfefe860479e2c1742f2bd6a32
Alex.Kapajika Ntumba <alex.kapajika.ntumba@stu.ocsb.ca>
2024-01-12 13:11:43 -0500

    CPT-07

--- code.py
+++ code.py
@@ -2,6 +2,43 @@ import ugame
 import stage
 import constants
 
+def menu_scene():
+
+
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+
+    text - []
+    text = stage.Text(width=29, height=12, font=None, palette=constants.RED_PALETTE,
+buffer=None)
+    text1.move(20,10)
+    text1.text("MT Game Studios")
+    text.append(text1)
+
+    text2 = stage.Text(width=29, height=12, font=None, palette=constants.RED_PALETTE,
+buffer=None)
+    text2.move(40, 110)
+    text("PRESS START")
+    text.append(text2)
+
+    background = stage.Grid(image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y)
+
+
+    game = stage.Stage(ugame.display, constants.FPS)
+
+    game.layers = text + [background]
+
+    game.render_block()
+
+    while True:
+
+        keys = ugame.buttons.get_pressed()
+
+        if keys & ugame.K_START != 0:
+            print("Start")
+
+
+        game.tick()
+
 def game_scene():
 
 
@@ -39,8 +76,8 @@ def game_scene():
         if keys & ugame.K_X:
             pass
         if keys & ugame.K_O != 0:
-            if a_button == constants.button_state["button up"]:
-                a_button = constants.button_state["button just_pressed"]
+            if a_button == constants.button_state["button_up"]:
+                a_button = constants.button_state["button_just_pressed"]
             elif a_button == constants.button_state["button_just_pressed"]:
                 a_button = constants.button_state["button_still_pressed"]
         else:
@@ -75,5 +112,7 @@ def game_scene():
         game.render_sprites([ship] + [alien])
         game.tick()
 
+
+
 if __name__ == "__main__":
-    game_scene()
+    menu_scene()
